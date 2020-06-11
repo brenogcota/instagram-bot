b71e4af8e87d3ebef3fc58b53ac135e16a8d85b1
brenogcota <brenocota4@gmail.com>
2020-06-11 00:45:00 -0300

    clear data

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const insta = require('./insta');
 
 (async () => {
 
-    const USER_NAME = 'dbrno';
-    const PASSWORD = 'brenobrl';
+    const USER_NAME = '';
+    const PASSWORD = '';
     await insta.initialize();
     await insta.login(USER_NAME, PASSWORD);
 
